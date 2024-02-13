fef3bf83f2828c4eebd8e515aab9c771f146ca27
Biladhar <laadharbilel09@gmail.com>
2024-02-13 23:21:46 +0100

    Portfolio Project

---
+++ script.js
@@ -0,0 +1,16 @@
+// dark mode 
+
+function addDarkmodeWidget() {
+    const options = {
+        time: '0.5s', // default: '0.3s'
+        mixColor: '#fff', // default: '#fff'
+        backgroundColor: '#fff',  // default: '#fff'
+        buttonColorDark: '#100f2c',  // default: '#100f2c'
+        buttonColorLight: '#fff', // default: '#fff'
+        saveInCookies: false, // default: true,
+        label: '🌓', // default: ''
+        autoMatchOsTheme: true // default: true
+    }
+    new Darkmode(options).showWidget();
+}
+window.addEventListener('load', addDarkmodeWidget);
