dfeff22ee75524123471a30bd5a4af4028ace304
Biladhar <laadharbilel09@gmail.com>
2024-02-15 03:42:05 +0100

    i added a groupe section with a fuction to show for every member his skills and a box with his narrative

--- script.js
+++ script.js
@@ -26,3 +26,72 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+
+var htmlNum = [30, 20, 90, 60, 60];
+var cssNum = [43, 66, 63, 23, 50];
+var jsNum = [40, 60, 56, 40, 80];
+
+function showSkills(i){
+    
+
+    document.getElementById('skillsBox').style.display="-webkit-box"
+    
+    const najemText ="najemText";
+    const yassinText ="yassinText";
+    const bilelText ="bilelText";
+    const ihebText ="ihebText";
+    const OmarText ="OmarText";
+
+
+    switch (i) {
+        case 0:
+            document.getElementById('narrative').innerText=najemText;
+            document.getElementById('htmlValue').innerText = htmlNum[0] + "%";
+            document.getElementById('htmlbg').style.width = htmlNum[0] + "%";
+            document.getElementById('cssValue').innerText = cssNum[0] + "%";
+            document.getElementById('cssbg').style.width = cssNum[0] + "%";
+            document.getElementById('jsValue').innerText = jsNum[0] + "%";
+            document.getElementById('jsbg').style.width = jsNum[0] + "%";
+            break;
+        case 1:
+            document.getElementById('narrative').innerText=yassinText;
+            document.getElementById('htmlValue').innerText = htmlNum[1] + "%";
+            document.getElementById('htmlbg').style.width = htmlNum[1] + "%";
+            document.getElementById('cssValue').innerText = cssNum[1] + "%";
+            document.getElementById('cssbg').style.width = cssNum[1] + "%";
+            document.getElementById('jsValue').innerText = jsNum[1] + "%";
+            document.getElementById('jsbg').style.width = jsNum[1] + "%";
+            break;
+        case 2:
+            document.getElementById('narrative').innerText=bilelText;
+            document.getElementById('htmlValue').innerText = htmlNum[2] + "%";
+            document.getElementById('htmlbg').style.width = htmlNum[2] + "%";
+            document.getElementById('cssValue').innerText = cssNum[2] + "%";
+            document.getElementById('cssbg').style.width = cssNum[2] + "%";
+            document.getElementById('jsValue').innerText = jsNum[2] + "%";
+            document.getElementById('jsbg').style.width = jsNum[2] + "%";
+            break;
+        case 3:
+            document.getElementById('narrative').innerText=ihebText;
+            document.getElementById('htmlValue').innerText = htmlNum[3] + "%";
+            document.getElementById('htmlbg').style.width = htmlNum[3] + "%";
+            document.getElementById('cssValue').innerText = cssNum[3] + "%";
+            document.getElementById('cssbg').style.width = cssNum[3] + "%";
+            document.getElementById('jsValue').innerText = jsNum[3] + "%";
+            document.getElementById('jsbg').style.width = jsNum[3] + "%";
+            break;
+        case 4:
+            document.getElementById('narrative').innerText=OmarText;
+            document.getElementById('htmlValue').innerText = htmlNum[4] + "%";
+            document.getElementById('htmlbg').style.width = htmlNum[4] + "%";
+            document.getElementById('cssValue').innerText = cssNum[4] + "%";
+            document.getElementById('cssbg').style.width = cssNum[4] + "%";
+            document.getElementById('jsValue').innerText = jsNum[4] + "%";
+            document.getElementById('jsbg').style.width = jsNum[4] + "%";
+            break;
+                  
+        default:
+          statements
+      }
+}
